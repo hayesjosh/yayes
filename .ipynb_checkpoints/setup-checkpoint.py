488e0cb65b1af385834b7b0f9528001530032bb1
Greg Yannett <gsyann@berkeley.edu>
2019-09-18 23:38:27 -0700

    V1 Complete

--- .ipynb_checkpoints/setup-checkpoint.py
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -1,8 +1,13 @@
-from setuptools import setup
+from setuptools import setup, Extension
+
+from os import path
+this_directory = path.abspath(path.dirname("yayes"))
+with open(path.join(this_directory, 'README-pypi.md'), encoding='utf-8') as f:
+    long_description = f.read()
     
 setup(name='yayes',
-      version='1.0.1',
-      Description='Repair erroneous entries in data streams while maintaining accuracy of overall trends.',
+      version='1.0.5',
+      description='Repair erroneous entries in data streams while maintaining accuracy of overall trends.',
       url='https://github.com/hayesjosh/yayes',
       author='Greg Yannett & Josh Hayes',
       author_email='gsyann@berkeley.edu, hayesjosh@alumni.stanford.edu',
@@ -13,4 +18,6 @@ setup(name='yayes',
                    'Programming Language :: Python'],
       keywords='yayes smoothing data error fill patching trend Greg Yannett Josh Hayes',
       include_package_data=True,
-      zip_safe=True)
+      zip_safe=True,
+      long_description=long_description,
+      long_description_content_type='text/markdown')
